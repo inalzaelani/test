b4afe969b3c81bea13293882242316aff4c9c0bd
inalzaelani <107565500+inalzaelani@users.noreply.github.com>
2022-11-16 12:37:57 +0700

    master

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -41,7 +41,7 @@
         <ul class="navbar-nav navbar-right">
           <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
             <img alt="image" src="../assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-            <div class="d-sm-none d-lg-inline-block">Hi, {{ $user->username }}</div></a>
+            <div class="d-sm-none d-lg-inline-block">Hallo, {{ $user->username }}!</div></a>
             <div class="dropdown-menu dropdown-menu-right">
               <div class="dropdown-divider"></div>
               <a href="/postlogout" class="dropdown-item has-icon text-danger">
